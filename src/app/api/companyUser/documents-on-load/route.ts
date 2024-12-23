d21dacc527281e553bb8a118dcabc34d21826645
Yuvraj <yuvrajs22it@student.mes.ac.in>
2024-12-24 00:04:25 +0530

    bug fixes v2.

--- src/app/api/companyUser/documents-on-load/route.ts
+++ src/app/api/companyUser/documents-on-load/route.ts
@@ -1,32 +1,34 @@
-// /* eslint-disable @typescript-eslint/no-explicit-any */
-// import DocumentModel from "@/model/Documents.model";
-// import { cookies } from "next/headers";
-// import { NextRequest, NextResponse } from "next/server";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import DocumentModel from "@/model/Documents.model";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
 
-// export async function GET(request: NextRequest) {
-//   try {
-//     // const { searchParams } = request.nextUrl;
+export async function GET(request: NextRequest) {
+  try {
+    const { searchParams } = request.nextUrl;
 
-//     await DocumentModel.aggregate([
-//       {
-//         $lookup: {
-//           from: "users",
-//           localField: "authorId",
-//           foreignField: "_id",
-//           as: "documents",
-//         },
-//       },
-//     ]);
+    console.log(searchParams);
 
-//     const cookiesStore = await cookies();
-//     const token = cookiesStore.get("login-user-005");
+    await DocumentModel.aggregate([
+      {
+        $lookup: {
+          from: "users",
+          localField: "authorId",
+          foreignField: "_id",
+          as: "documents",
+        },
+      },
+    ]);
 
-//     console.log(token?.value);
-//   } catch (error: any) {
-//     return NextResponse.json({
-//       status: 500,
-//       message: error.message || "Internal Server Error",
-//       success: false,
-//     });
-//   }
-// }
+    const cookiesStore = await cookies();
+    const token = cookiesStore.get("login-user-005");
+
+    console.log(token?.value);
+  } catch (error: any) {
+    return NextResponse.json({
+      status: 500,
+      message: error.message || "Internal Server Error",
+      success: false,
+    });
+  }
+}
